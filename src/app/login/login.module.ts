0cd256e47e69579a510a0cd505b67f9c9eae2944
Mathias Remshardt <mathias.remshardt@logicline.de>
2020-08-22 12:22:22 +0200

    * adds routing from login to main

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
+import { LoginComponent } from './login.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent],
   imports: [CommonModule, LoginRoutingModule],
 })
 export class LoginModule {}
