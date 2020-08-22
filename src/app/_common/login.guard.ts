691e80af597bc18eff0c27db8acc2ee2175d0267
Mathias Remshardt <mathias.remshardt@logicline.de>
2020-08-22 12:40:38 +0200

    * adds login guard

---
+++ src/app/_common/login.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { LoginService } from './login.service';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginGuard implements CanActivate {
+  constructor(private loginService: LoginService, private router: Router) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> {
+    return this.loginService.isLoggedIn$.pipe(
+      map((loggedIn) => loggedIn || this.router.parseUrl('/login'))
+    );
+  }
+}
--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main.component';
+import { LoginGuard } from '../_common/login.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: MainComponent,
+    canActivate: [LoginGuard],
   },
 ];
 
